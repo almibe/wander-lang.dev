3e6c6bdf4f0b0ef22c6a18835e9bd6ebb52742d7
Alex Michael Berry <alexmiberry@gmail.com>
2023-10-16 02:04:47 -0400

    adding mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [
-            wasm(),
-            topLevelAwait()
-          ]        
-    }
+  vite: {
+    plugins: [wasm(), topLevelAwait()]
+  },
+  integrations: [mdx()]
 });
